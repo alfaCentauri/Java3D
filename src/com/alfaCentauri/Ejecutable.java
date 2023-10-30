e1467df7213d46b48711c5302017622258dfdc90
Ricardo Presilla <presilla.r@opendevpro.com>
2023-10-30 13:56:54 -0300

    Add window

--- src/com/alfaCentauri/Ejecutable.java
+++ src/com/alfaCentauri/Ejecutable.java
@@ -1,6 +1,42 @@
 package com.alfaCentauri;
 
-public class Ejecutable {
+import java.awt.*;
+
+public class Ejecutable extends javax.swing.JFrame {
+
+    private javax.swing.JPanel Panel;
+
+    public static final int HEIGHT = 800;
+    public static final int WIDTH = 600;
+    public static final String TITLE = "Ejemplo de Java 3D.";
+
+    /**
+     * Constructs a new frame that is initially invisible.
+     * <p>
+     * This constructor sets the component's locale property to the value
+     * returned by <code>JComponent.getDefaultLocale</code>.
+     *
+     * @throws HeadlessException if GraphicsEnvironment.isHeadless()
+     *                           returns true.
+     * @see GraphicsEnvironment#isHeadless
+     * @see Component#setSize
+     * @see Component#setVisible
+     */
+    public Ejecutable() throws HeadlessException {
+        initComponents();
+        this.setTitle("Grafica de pruebas.");
+    }
+
+    /**
+     * Inicio de componentes.
+     **/
+    private void initComponents() {
+        Panel = new javax.swing.JPanel();
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Graficas");
+
+        Panel.setBackground(new java.awt.Color(186, 175, 155));
+    }
 
     /***/
     public static void main(String []args) {
